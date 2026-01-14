3f3ba596c3416d5a05f32d87232e14f95ae97961
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-14 16:09:37 +0500

    Do Homework9

---
+++ Homework9.py
@@ -0,0 +1,8 @@
+string_with_name = '"Nikita"'
+string_with_hobby = "'Videogames'"
+string_with_favorite_movie = """My favorite movie is
+28 Days Later"""
+
+print(string_with_name)
+print(string_with_hobby)
+print(string_with_favorite_movie)
