e4400769b2ff9ac84a5b0cf2f501edddcdb2ab46
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-14 17:31:54 +0500

    Do Homework13

---
+++ Homework13.py
@@ -0,0 +1,11 @@
+strip_example = "    strip   "
+print(strip_example.strip())
+
+replace_example = "Today is cold"
+print(replace_example.replace("cold", "warm"))
+
+split_example = "Python is great"
+print(split_example.split(" "))
+
+join_example = ["It's", "nice", "today"]
+print(" ".join(join_example))
