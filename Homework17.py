4071d49b953ae755cb261cc3b26526ee4f65162d
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-20 11:29:34 +0300

    Do Homework17.py

--- Homework17.py
+++ Homework17.py
@@ -1,5 +1,5 @@
 def max_number(a, b):
-    pass
+    return (a + b + abs(a - b)) / 2
 
 
 def empty_function():
@@ -7,8 +7,18 @@ def empty_function():
 
 
 def even_numbers(n):
-    pass
+    for i in range(0, n + 1, 2):
+        yield i
 
 
 def test_max_number():
-    pass
+    assert max_number(1, 2) == 2, "Ошибка: максимальное число из (1, 2) является 2"
+    assert max_number(6, 3) == 6, "Ошибка: максимальное число из (6, 3) является 6"
+    assert max_number(6, 6) == 6, "Ошибка: максимальное число из (6, 6) является 6"
+    print("Тесты пройдены")
+
+
+for x in even_numbers(11):
+    print(x)
+
+test_max_number()
