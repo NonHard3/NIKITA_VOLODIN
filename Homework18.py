33c25acebd599f10a3b46fdc73f913b54ef60a83
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-20 14:35:54 +0300

    Do Homework18.py

--- Homework18.py
+++ Homework18.py
@@ -3,8 +3,10 @@ def get_access_voting(age, is_citizen, is_disqualified):
         print("Вы допущены к голосованию\n")
     else:
         print("В доступе к голосованию отказано! Так как:")
-        if age < 18:
+        if age < 18 and age >= 0:
             print("- Вы несовершеннолетний")
+        else:
+            print("- Введен некорректный возраст")
         if not is_citizen:
             print("- У вас нет гражданства")
         if is_disqualified:
@@ -14,6 +16,7 @@ def get_access_voting(age, is_citizen, is_disqualified):
 def test_get_access_voting():
     get_access_voting(25, True, False)
     get_access_voting(15, True, False)
+    get_access_voting(-15, True, False)
     get_access_voting(18, False, False)
     get_access_voting(25, True, True)
     get_access_voting(15, False, True)
