da0857185501ad6989bfdb9d78293636d91302f9
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-21 10:56:56 +0300

    Update Homework18.py

--- Homework18.py
+++ Homework18.py
@@ -11,11 +11,50 @@ def get_access_voting(age, is_citizen, is_disqualified):
             print("- Что-то с вами не так")
         print()
 
-def test_get_access_voting():
-    get_access_voting(25, True, False)
-    get_access_voting(15, True, False)
-    get_access_voting(18, False, False)
-    get_access_voting(25, True, True)
-    get_access_voting(15, False, True)
 
-test_get_access_voting()
+def input_age():
+    global age
+    try:
+        value = int(input("Введите ваш возраст: "))
+    except ValueError:
+        print("Введено нечисловое значение! Попробуйте ещё раз")
+        input_age()
+    else:
+        age = value
+
+
+def input_citizen():
+    global is_citizen
+    answer = input("""У вас есть гражданство?
+Введите Да или Нет: """)
+    if answer == "Да" or answer == "да":
+        is_citizen = True
+    elif answer == "Нет" or answer == "нет":
+        is_citizen = False
+    else:
+        print("Неверное значение!")
+        input_citizen()
+
+
+def input_disqualified():
+    global is_disqualified
+    answer = input("""У вас есть/была судимость?
+Введите Да или Нет: """)
+    if answer == "Да" or answer == "да":
+        is_disqualified = True
+    elif answer == "Нет" or answer == "нет":
+        is_disqualified = False
+    else:
+        print("Неверное значение!")
+        input_disqualified()
+
+
+age = 0
+is_citizen = None
+is_disqualified = None
+
+input_age()
+input_citizen()
+input_disqualified()
+
+get_access_voting(age, is_citizen, is_disqualified)
