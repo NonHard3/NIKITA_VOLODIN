ec77fa00d2366fa663fed09c1f07a422844d04f3
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-22 10:15:59 +0300

    Update Homework19.py

--- Homework19.py
+++ Homework19.py
@@ -1,21 +1,19 @@
 def convert_number_to_text(number):
-    if 0 < number <= 5:
-        text = ""
-        if number == 1:
-            text = "One"
-        if number == 2:
-            text = "Two"
-        if number == 3:
-            text = "Three"
-        if number == 4:
-            text = "Four"
-        if number == 5:
-            text = "Five"
-        print("Соответствующее слово: " + text)
+    if number == 1:
+        text = "One"
+    elif number == 2:
+        text = "Two"
+    elif number == 3:
+        text = "Three"
+    elif number == 4:
+        text = "Four"
+    elif number == 5:
+        text = "Five"
     else:
         print("Число вне диапазона! Попробуйте ещё раз")
         input_number()
-
+        return
+    print("Соответствующее слово: " + text)
 
 def input_number():
     try:
