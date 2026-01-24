b3224b810f1182b667b7f379f11609a9567ad1ee
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-24 12:35:47 +0300

    Update Homework22.py

--- Homework22.py
+++ Homework22.py
@@ -23,28 +23,32 @@ def count_word_repetitions(current_word, dict_type):
         dict_type[current_word] = 1
 
 
-word_count = 0
-longest_word = ""
-vowel_count = 0
-dict_words = {}
+def analyze_the_text():
+    word_count = 0
+    longest_word = ""
+    vowel_count = 0
+    dict_words = {}
 
-text = set_lower_style_and_erase_punctuation(input("Введите текст:\n"))
+    text = set_lower_style_and_erase_punctuation(input("Введите текст:\n"))
 
-for word in text.split(" "):
-    count_word_repetitions(word, dict_words)
+    for word in text.split(" "):
+        count_word_repetitions(word, dict_words)
 
-    longest_word = find_longest_word(word, longest_word)
+        longest_word = find_longest_word(word, longest_word)
 
-    word_count += 1
+        word_count += 1
 
-for char in text:
-    if char in "аеёиоуыэюя":
-        vowel_count += 1
+    for char in text:
+        if char in "аеёиоуыэюя":
+            vowel_count += 1
 
-print(f"Количество слов в тексте: {word_count}")
-print(f"Самое длинное слово: {longest_word}")
-print(f"Количество гласных в тексте: {vowel_count}")
-print("Количество повторений каждого слова:")
+    print(f"Количество слов в тексте: {word_count}")
+    print(f"Самое длинное слово: {longest_word}")
+    print(f"Количество гласных в тексте: {vowel_count}")
+    print("Количество повторений каждого слова:")
 
-for key, value in dict_words.items():
-    print(f"{key}: {value}")
+    for key, value in dict_words.items():
+        print(f"{key}: {value}")
+
+
+analyze_the_text()
