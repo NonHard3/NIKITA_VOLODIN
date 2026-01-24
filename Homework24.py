5a2c3bd6a33ff8b33dc28f4e271db9ea442ebdcf
NonHard3 <165911721+NonHard3@users.noreply.github.com>
2026-01-24 12:26:17 +0300

    Update Homework24.py

--- Homework24.py
+++ Homework24.py
@@ -1,18 +1,17 @@
 def swap_first_and_last(list_type):
-    first = list_type[0]
-    last = list_type[-1]
-    list_type[0] = last
-    list_type[-1] = first
+    list_type[0], list_type[-1] = list_type[-1], list_type[0]
     return list_type
 
+def create_list_str():
+    list_str = []
 
-list_str = []
+    print("Введите 5 значений:\n")
 
-print("Введите 5 значений:\n")
+    for _ in range(5):
+        list_str.append(input())
 
-for _ in range(5):
-    list_str.append(input())
+    return list_str
 
-swap_first_and_last(list_str)
+result = swap_first_and_last(create_list_str())
 
-print(list_str)
+print(result)
